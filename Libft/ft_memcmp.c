cccd82dcc6a02ed2075f18ad05207a316ac250da
Ignacio Navarro Poves <inavarro@c3r1s3.42malaga.com>
2022-10-21 11:42:41 +0200

    added function to mf and lib

---
+++ Libft/ft_memcmp.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inavarro <inavarro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/21 11:37:48 by inavarro          #+#    #+#             */
+/*   Updated: 2022/10/21 11:39:24 by inavarro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	
+}
+
+/*
+The memcmp() function compares byte string s1 against byte string s2.
+Both strings are assumed to be n bytes long.
+
+The memcmp() function returns zero if the two strings are identical, oth-
+erwise returns the difference between the first two differing bytes
+(treated as unsigned char values, so that `\200' is greater than `\0',
+for example).  Zero-length strings are always identical.  This behavior
+is not required by C and portable code should only depend on the sign of
+the returned value.
+*/
