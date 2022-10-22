cc189d17cb046782270533da237b7a35f9e14a81
iggynp <odissey0x@gmail.com>
2022-10-22 16:49:16 +0200

    added more functions

--- Libft/ft_memcmp.c
+++ Libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: inavarro <inavarro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 11:37:48 by inavarro          #+#    #+#             */
-/*   Updated: 2022/10/21 11:39:24 by inavarro         ###   ########.fr       */
+/*   Updated: 2022/10/22 16:48:20 by inavarro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,16 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (((unsigned char *)s1)[i] != (((unsigned char *)s2)[i]))
+			return (((unsigned char *)s1)[i] - (((unsigned char *)s2)[i]));
+		i++;
+	}
+	return (0);
 }
 
 /*
