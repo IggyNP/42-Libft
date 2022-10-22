5548736a36fceb8f6208377bdd0cbb1c5fe661e6
iggynp <odissey0x@gmail.com>
2022-10-22 22:16:51 +0200

    added more functions

--- Libft/ft_atoi.c
+++ Libft/ft_atoi.c
@@ -6,15 +6,35 @@
 /*   By: inavarro <inavarro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 19:30:15 by inavarro          #+#    #+#             */
-/*   Updated: 2022/10/22 19:43:26 by inavarro         ###   ########.fr       */
+/*   Updated: 2022/10/22 22:16:25 by inavarro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-	hey;
+	long	s;
+	long	res;
+	int		i;
+
+	s = 1;
+	res = 0;
+	i = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			s *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = (str[i] - '0') + (res * 10);
+		i++;
+	}
+	return ((int)res * s);
 }
 
 /*
--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: inavarro <inavarro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 11:48:52 by inavarro          #+#    #+#             */
-/*   Updated: 2022/10/22 19:37:22 by inavarro         ###   ########.fr       */
+/*   Updated: 2022/10/22 21:51:21 by inavarro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,6 @@ int			ft_strncmp(const char *s1, const char *s2, size_t n);
 void		*ft_memchr(const void *s, int c, size_t n);
 int			ft_memcmp(const void *s1, const void *s2, size_t n);
 char		*ft_strnstr(const char *haystack, const char *needle, size_t len);
-int			atoi(const char *str);
+int			ft_atoi(const char *str);
 
 #endif
