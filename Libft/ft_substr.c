0f3bd2064de3dac2929646b621cb5653561bb16a
Ignacio Navarro Poves <inavarro@c3r3s2.42malaga.com>
2022-10-25 17:42:54 +0200

    prueba

---
+++ Libft/ft_substr.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inavarro <inavarro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/25 16:34:51 by inavarro          #+#    #+#             */
+/*   Updated: 2022/10/25 17:42:41 by inavarro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+hey
+}
+
+/*
+Allocates (with malloc(3)) and returns a substring
+from the string ’s’.
+The substring begins at index ’start’ and is of
+maximum size ’len’.
+
+Return value: The substring.
+NULL if the allocation fails.
+*/
