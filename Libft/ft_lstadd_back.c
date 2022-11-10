d2854f0c29db01f57f4160109c0d686edf102dc4
iggynp <odissey0x@gmail.com>
2022-11-10 18:37:39 +0100

    added more functions

---
+++ Libft/ft_lstadd_back.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inavarro <inavarro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/10 17:51:38 by inavarro          #+#    #+#             */
+/*   Updated: 2022/11/10 18:36:04 by inavarro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*temp;
+
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	temp = ft_lstlast(*lst);
+	temp->next = new;
+}
+
+/*
+Adds the node ’new’ at the end of the list.
+*/
