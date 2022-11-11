df13793931079824b3ca37b63be1c26bf07dbd52
iggynp <odissey0x@gmail.com>
2022-11-11 09:32:36 +0100

    added more functions

--- Libft/ft_lstmap.c
+++ Libft/ft_lstmap.c
@@ -6,17 +6,38 @@
 /*   By: inavarro <inavarro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 19:42:17 by inavarro          #+#    #+#             */
-/*   Updated: 2022/11/10 19:46:57 by inavarro         ###   ########.fr       */
+/*   Updated: 2022/11/11 09:30:12 by inavarro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *),void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
+	t_list	*new;
+	t_list	*temp;
 
+	if (!lst)
+		return (0);
+	new = 0;
+	while (lst)
+	{
+		temp = ft_lstnew(f(lst->content));
+		if (!temp)
+		{
+			ft_lstclear(&new, del);
+			return (0);
+		}
+		ft_lstadd_back(&new, temp);
+		lst = lst->next;
+	}
+	return (new);
 }
 
 /*
-
+Iterates the list ’lst’ and applies the function
+’f’ on the content of each node. Creates a new
+list resulting of the successive applications of
+the function ’f’. The ’del’ function is used to
+delete the content of a node if needed.
 */
